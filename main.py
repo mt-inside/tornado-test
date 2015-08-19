f482959fc3f756144a8fae5c7bf25f4f58fc7161
Matt Turner <matttur@cisco.com>
2015-08-19 10:13:40 +0000

    First

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import tornado.web
+import tornado.ioloop
+import json
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world!")
+
+class ThingHandler(tornado.web.RequestHandler):
+    def get(self, parent_id, child_id):
+        self.write("Hello, %s, you're interested in %s!\n" % (parent_id, child_id))
+        self.write("And you gave the 'hello' argument as %s\n" % self.get_argument("hello"))
+
+    def post(self, parent_id, child_id):
+        self.write("Hello, %s, you're interested in %s!\n" % (parent_id, child_id))
+        data = json.loads(self.request.body.decode('utf-8'))
+        self.write("And you have the 'hello' datum as %s\n" % data['hello'])
+
+
+application = tornado.web.Application([
+    (r"/", IndexHandler),
+    (r"/v1/parent/([0-9]*)/child/?([0-9]*)", ThingHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
